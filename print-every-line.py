91b529dbe0c4e1d45711f2e8df559ef3b4e756f9
thuan06 <thuan0610@gmail.com>
2017-06-30 00:06:44 +0700

    Add beginer commit

---
+++ print-every-line.py
@@ -0,0 +1,4 @@
+print "This is the nth times start learning Python"
+print 'Try harder for this time. Pull code everday'
+print "Firstly, just try some new as: git, pycharm,.. easy install and how to use them"
+print "Try push to github.com with my account via this code interface"
