33c34eb8c0843c89c9f00b8f8fa607d5523ca624
Dương Đình Thuận <thuandd@appota.com>
2017-06-30 00:13:41 +0700

    Add beginer commit

--- print-every-line.py
+++ print-every-line.py
@@ -2,3 +2,5 @@ print "This is the nth times start learning Python"
 print 'Try harder for this time. Pull code everday'
 print "Firstly, just try some new as: git, pycharm,.. easy install and how to use them"
 print "Try push to github.com with my account via this code interface"
+print "Now, everything seem okey, just try: Ctrl + Shift + K and allow push"
+print " No no, it seem that i haven't push new line to github yet"
