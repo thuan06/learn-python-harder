254be9e3fe9b080e02b368a236fd9ad034f4e1fe
thuan06 <thuan0610@gmail.com>
2017-06-30 00:28:05 +0700

    Try multi line printe

---
+++ print-multiline.py
@@ -0,0 +1,10 @@
+print """1. this is multi line test
+          weather it can print in multi line
+          if i write by this o
+    or may be return an error code """
+print """2. an other try by combining with \
+        in the last of each line i enter \
+        so it will concat the previous line \
+        with this line"""
+print "Wow, in the first case, all thing print on corect order"
+print "In the second case, every line end by \ will be concated with previous line and add some space"
