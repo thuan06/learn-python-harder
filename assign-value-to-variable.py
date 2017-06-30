3aa4d8f843120ec049feee934074b21e2eeac689
thuan06 <thuan0610@gmail.com>
2017-07-01 00:15:37 +0700

    [2017-30-06] Get up with variable

---
+++ assign-value-to-variable.py
@@ -0,0 +1,38 @@
+counter = 10        # an interger number
+pi = 3.14           # float number
+name = "orion"      # string
+heigh=width=length=20
+
+print "counter: ", counter
+print "pi ", pi
+print "name: ", name
+print "heigh: ", heigh
+print "width: ", width
+print "length: ", length
+
+i1 = 1+0.5j
+i2= 2 - 3j
+complexsum = i1+i2
+print i1
+print i2
+print complexsum
+
+hexagon1= 0o70
+print "base 10: ", hexagon1 ,"= "
+print hex(hexagon1) ,"in base 8"
+
+bin1 = 0b10010
+print bin1
+print bin(bin1)
+
+# String variable area
+
+tusuong1 ="Thuan dep trai"
+conlai = " vo doi"
+print tusuong1
+print conlai
+print tusuong1[::2] # in cac ki tu cach nhau 1
+print conlai[-1:] # in ki tu cuoi cung -1 = last --> last <=conlai[-1]<conlai[0]
+print tusuong1*2 # ---> in ra 2 lan
+print tusuong1+conlai # --> in ra full bo
+
